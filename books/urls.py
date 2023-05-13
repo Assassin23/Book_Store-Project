604543ffcff3d468862424163f322e4dfef97e83
Aditya <aditya332233@gmail.com>
2023-05-13 11:18:07 +0530

    Added searching capabilities to the app.

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchListView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
     path("<uuid:pk>", BookDetailView.as_view(), name="book_detail"),
+    path("search/", SearchListView.as_view(), name="search_results"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,10 @@
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models.query import QuerySet
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
+
 from .models import Book
 
 
@@ -9,9 +14,24 @@ class BookListView(LoginRequiredMixin, ListView):
     template_name = "books/book_list.html"
     login_url = "account_login"
 
+
 class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = "book"
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class SearchListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        query = self.request.GET.get("q")
+        return (
+            super()
+            .get_queryset()
+            .filter(Q(title__icontains=query) | Q(author__icontains=query))
+        )
